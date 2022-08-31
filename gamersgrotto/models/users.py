1385edc60501aef473e70b7afd0857c8fabf609c
Ismael Ali <ismaelali78@outlook.com>
2022-08-31 12:07:16 +0100

    changed password column in models for users

--- gamersgrotto/models/users.py
+++ gamersgrotto/models/users.py
@@ -2,11 +2,12 @@ from ..database.db import db
 
 # create db model for user auth
 
+
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(300), nullable=False)
     # date = db.Column(db.String(100), nullable=False)
     # full_name = db.Column(db.String(100), nullable=True)
