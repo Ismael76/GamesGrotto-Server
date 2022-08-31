aac00c63016089c11e2737c9c43e2af17de67a38
Ismael Ali <ismaelali78@outlook.com>
2022-08-31 12:36:32 +0100

    pushing to deploy

--- gamersgrotto/models/users.py
+++ gamersgrotto/models/users.py
@@ -8,6 +8,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True, nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
-    password = db.Column(db.String(300), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     # date = db.Column(db.String(100), nullable=False)
     # full_name = db.Column(db.String(100), nullable=True)
