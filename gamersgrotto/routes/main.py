e904f5499af30e9ce9826f33333ae98111e01b10
Ismael Ali <ismaelali78@outlook.com>
2022-08-31 09:55:26 +0100

    added main route

--- gamersgrotto/routes/main.py
+++ gamersgrotto/routes/main.py
@@ -6,13 +6,8 @@ main_routes = Blueprint("main", __name__)
 
 # these will be the main app routes for game database CRUD.
 
+
 @main_routes.route("/", methods=["GET", "POST"])
 def index():
-
     if request.method == "GET":
-
-        pass
-
-    else:
-
-        pass
+        return "Games Grotto API"
